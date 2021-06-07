17dde49f66e921ccf827f89bbafa008bc31448af
fraserpage <fraserpage@gmail.com>
2021-06-07 01:06:37 -0400

    add to library

--- library.js
+++ library.js
@@ -1,5 +1,6 @@
 export const libraryContent = {
     "r-pentomino": {
+        url: "https://conwaylife.com/wiki/R-pentomino",
         name: "R-pentomino",
         coords: [
             [0,1],
@@ -13,3 +14,91 @@ export const libraryContent = {
     }
 }
 
+export const txtPatternLibrary = [ 
+
+`!Name: Toaster
+!Author: Dean Hickerson
+!A period 5 oscillator found in April 1992 that produces sparks similar to those of a lightweight spaceship.
+!URL: https://conwaylife.com/wiki/Toaster
+..O......OO
+.O.O.OO..O
+.O.O.O.O.O
+OO.O...O.OO
+O..OO.OO..O
+.O.......O
+.O.......O
+O..OO.OO..O
+OO.O...O.OO
+.O.O.O.O.O
+.O.O.OO..O
+..O......OO`,
+
+`!Name: Gosper glider gun
+!Author: Bill Gosper
+!The first known gun and the first known finite pattern with unbounded growth.
+!URL: https://www.conwaylife.com/wiki/Gosper_glider_gun
+........................O
+......................O.O
+............OO......OO............OO
+...........O...O....OO............OO
+OO........O.....O...OO
+OO........O...O.OO....O.O
+..........O.....O.......O
+...........O...O
+............OO`,
+
+`!Name: Nonomino switch engine
+!URL: https://conwaylife.com/wiki/Nonomino_switch_engine_predecessor
+OOO...
+..O.O.
+..OOOO`,
+
+`!Name: Lightweight spaceship
+!Author: John Conway
+!The smallest known orthogonally moving spaceship, and the second most common spaceship (after the glider).
+!URL: https://conwaylife.com/wiki/Lightweight_spaceship
+.O..O
+O
+O...O
+OOOO`,
+
+`!Name: Space rake
+!An orthogonal period 20 c/2 forward glider rake.
+!URL: https://conwaylife.com/wiki/Space_rake
+...........OO.....OOOO
+.........OO.OO...O...O
+.........OOOO........O
+..........OO.....O..O
+.
+........O
+.......OO........OO
+......O.........O..O
+.......OOOOO....O..O
+........OOOO...OO.OO
+...........O....OO
+.
+.
+.
+..................OOOO
+O..O.............O...O
+....O................O
+O...O............O..O
+.OOOO`,
+
+`!Name: Loafer
+! loafer.cells
+! Josh Ball
+! small c/7 orthogonal spaceship found 17 February 2013
+! https://www.conwaylife.com/forums/viewtopic.php?f=2&t=1031#p7450
+!URL: https://conwaylife.com/wiki/Loafer
+.OO..O.OO
+O..O..OO.
+.O.O.....
+..O......
+........O
+......OOO
+.....O...
+......O..
+.......OO`
+
+]
